98faff52ee9997c33c31266bc3c68d93f9b4d4b7
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2024-04-23 17:19:29 -0400

    fix: segmentation fault due

--- src/format.cpp
+++ src/format.cpp
@@ -17,7 +17,7 @@ string Format::ElapsedTime(long seconds) {
     const auto hrs = std::chrono::duration_cast<std::chrono::hours>(durationInSecs);
     const auto mins = std::chrono::duration_cast<std::chrono::minutes>(durationInSecs - hrs);
     const auto secs = std::chrono::duration_cast<std::chrono::seconds>(durationInSecs - hrs - mins);
-    char* output;
+    char output[9];
     std::sprintf(output, "%.2li:%.2li:%.2li", hrs.count(), mins.count(), secs.count());
     return output;
 }
