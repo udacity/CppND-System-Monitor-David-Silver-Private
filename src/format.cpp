c0157a2565d472169fbdc435f1736efc3dec10eb
Stephen Jackson <stephen.lee.jackson+github@googlemail.com>
2024-04-23 12:24:35 -0400

    feat: implementing the elapsed time formatter

--- src/format.cpp
+++ src/format.cpp
@@ -1,11 +1,23 @@
 #include <string>
+#include <chrono>
+#include <cstdio>
 
 #include "format.h"
 
 using std::string;
 
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+/**
+ *  @brief Helper function for displaying a duration.
+ *  @param seconds duration measured in seconds
+ * 
+ *  @returns the duration formatted as HH:MM:SS
+ */
+string Format::ElapsedTime(long seconds) {
+    const std::chrono::seconds durationInSecs{seconds};
+    const auto hrs = std::chrono::duration_cast<std::chrono::hours>(durationInSecs);
+    const auto mins = std::chrono::duration_cast<std::chrono::minutes>(durationInSecs - hrs);
+    const auto secs = std::chrono::duration_cast<std::chrono::seconds>(durationInSecs - hrs - mins);
+    char* output;
+    std::sprintf(output, "%.2li:%.2li:%.2li", hrs.count(), mins.count(), secs.count());
+    return output;
+}
--- test/format_test.cpp
+++ test/format_test.cpp
@@ -3,4 +3,20 @@
 
 TEST(FormatTest, CorrectlyFormatsDataOnlySeconds) {
   EXPECT_EQ(Format::ElapsedTime(10), "00:00:10");
+  EXPECT_EQ(Format::ElapsedTime(1), "00:00:01");
+  EXPECT_EQ(Format::ElapsedTime(0), "00:00:00");
+  EXPECT_EQ(Format::ElapsedTime(59), "00:00:59");
+}
+
+TEST(FormatTest, CorrectlyFormatsDataMinutesAndSeconds) {
+  EXPECT_EQ(Format::ElapsedTime(61), "00:01:01");
+  EXPECT_EQ(Format::ElapsedTime(610), "00:10:10");
+  EXPECT_EQ(Format::ElapsedTime(3540), "00:59:00");
+  EXPECT_EQ(Format::ElapsedTime(3599), "00:59:59");
+}
+
+TEST(FormatTest, CorrectlyFormatsData) {
+  EXPECT_EQ(Format::ElapsedTime(3600), "01:00:00");
+  EXPECT_EQ(Format::ElapsedTime(3661), "01:01:01");
+  EXPECT_EQ(Format::ElapsedTime(356400), "99:00:00");
 }
