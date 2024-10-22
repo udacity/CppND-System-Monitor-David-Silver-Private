213f450e9b66a9eacf022d2aa0b528ad3a53a3ce
EdmundHussetl <jorge.pemjean.letelier@gmail.com>
2024-10-22 12:34:40 -0400

    feat(noent): finish processor implementation

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,15 @@
 #include "processor.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+#include <sstream>
+#include <string>
+
+#include "linux_parser.h"
+
+// DONE: Return the aggregate CPU utilization
+float Processor::Utilization() {
+  auto cpus = LinuxParser::CpuUtilization();
+  std::istringstream cpu_info(cpus.size() > 0 ? cpus[0] : "0 %");
+  std::string cpu_utilization;
+  cpu_info >> cpu_utilization;
+  return std::stof(cpu_utilization);
+};
